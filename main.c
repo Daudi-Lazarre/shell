00c525d35128f604867003463a75e13c2d4f6525
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-08-24 18:35:18 -0400

    space in for loop

--- main.c
+++ main.c
@@ -62,10 +62,10 @@ int _strcmp(char *s1, char *s2)
 void print_env(char **env)
 {
 	int i;
-	for(i = 0; env[i]; i++)
+	for (i = 0; env[i]; i++)
 		printf("env[%d]: %s\n", i, env[i]);
 }
-	
+
 /**
  * main - Entry point of simple shell program.
  *
