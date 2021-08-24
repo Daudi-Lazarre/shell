ea6617c626fb0f70ce55cfcf9f711332750eced8
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-08-24 17:02:35 -0400

    Added blocking operation to read line with $ prompt

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
+
 /**
  * main - PID
  *
@@ -16,11 +29,14 @@ int main(int ac __attribute__((unused)), char **av)
 	char *input = NULL;
 	size_t size;
 
-	printf("%u\n", myPid);
-	printf("%u\n", parentPid);
+	//printf("%u\n", myPid);
+	//printf("%u\n", parentPid);
 
 	while (1)
 	{
+		_putchar('$');
+		_putchar(' ');
+
 		if (getline(&input, &size, stdin) == -1)
 			printf("No line\n");
 		else
