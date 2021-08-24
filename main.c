b35cf8e0bf374cee83c40e920dcafdb13e028576
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-08-24 17:24:11 -0400

    added exit functionality

--- main.c
+++ main.c
@@ -15,6 +15,36 @@ int _putchar(char c)
 }
 
 /**
+ * new_prompt - starts a new prompt
+ */
+void new_prompt(void)
+{
+	_putchar('\n');
+	_putchar('$');
+	_putchar(' ');
+}
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: pointer to string 1
+ * @s2: pointer to string 2
+ *
+ * Return: -diff if 1 LT 2 0 if 1 EQ 2 +diff if 1 GT 2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int retval = 0;
+
+	while ((s1[i] && s2[i]) && (s1[i] == s2[i]))
+		i++;
+
+	retval = s1[i] - s2[i];
+
+	return (retval);
+}
+	
+/**
  * main - Entry point of simple shell program.
  *
  * Return: Always 0.
@@ -29,29 +59,25 @@ int main(int ac __attribute__((unused)), char **av)
 	char *input = NULL;
 	size_t size;
 
-	//printf("%u\n", myPid);
-	//printf("%u\n", parentPid);
+	printf("My PID: %u\n", myPid);
+	printf("Parent PID: %u\n", parentPid);
+
+	/* prints all argv values */
+	for(i = 0; av[i]; i++)
+		printf("av[%d]: %s\n", i, av[i]);
 	
-	/* do initial prompt */
-	_putchar('$');
-	_putchar(' ');
+	new_prompt();
 
 	while (getline(&input, &size, stdin) != -1)
 	{
+		/* check if exit command */
+		if (_strcmp(input, "exit\n") == 0)
+			exit(1);
 
-		printf("%s\n", input);
+		/* todo: check if env command */
 
-		// parse string
-		// is exit? 
-		// is wildcard?
-		
-		
-		/* prints all argv values */
-		for(i = 0; av[i]; i++)
-		{
-			printf("av[%d]: %s\n", i, av[i]);
-		}
-		exit(1);
+		printf("%s", input);
+		new_prompt();
 	}
 	return (0);
 }
