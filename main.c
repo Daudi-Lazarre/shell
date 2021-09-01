eccfb1d473dc8114057f82a6af743117b1f4041f
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-09-01 16:14:08 -0400

    implemented goodbye function

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include "holberton.h"
 
 /**
+ * goodbye - exits shell
+ * @input: input from getline
+ * @path: path variable double pointer
+ * @command: command parsed
+ */
+void goodbye(char *input, char **path, char **command)
+{
+	if (input)
+		free(input);
+	if (path)
+		free(path);
+	if (command)
+		free(command);
+
+	exit(0);
+}
+/**
  * main - Big boi shell
  * @ac: argument count
  * @av: argument vectors
@@ -19,18 +36,16 @@ int main(int ac __attribute__((unused)), char **av, char **env)
 	{
 		new_prompt();
 		if (getline(&input, &size, stdin) == -1)
-			exit_code = -1;
+		{
+			goodbye(input, path, NULL);
+		}
 		if (loop)
 		{
 			/* parse input to get command and arguments */
-			if (exit_code != -1)
-				command = parse_input(input, &size);
-			if ((_strcmp(command[0], "exit") == 0) || exit_code == -1)
+			command = parse_input(input, &size);
+			if ((_strcmp(command[0], "exit") == 0))
 			{
-				free(input);
-				free(path);
-				free(command);
-				exit(0);
+				goodbye(input, path, command);
 			}
 			exit_code = run_command(command, path, env, av);
 			free(command);
