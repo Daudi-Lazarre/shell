fdb9cc0f6832f51b5166c67ff8e1490570c1add2
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-09-01 15:24:07 -0400

    Changed puts to _puts

--- printenv.c
+++ printenv.c
@@ -1,12 +1,15 @@
 #include "holberton.h"
 /**
  * print_env - prints all environment variables
- * @env: environment variable list
+ * @env: environment variable list.
  */
 void print_env(char **env)
 {
 	int i;
 
 	for (i = 0; env[i]; i++)
+	{
 		_puts(env[i]);
+		_puts("\n");
+	}
 }
