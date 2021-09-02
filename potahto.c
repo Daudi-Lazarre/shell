d3ef2bb743bb7d76a35eb67e6415ba82a1cc817e
Weltopa <3447@holbertonschool.com>
2021-09-02 11:22:24 -0400

    Update potahto.c

--- potahto.c
+++ potahto.c
@@ -1,9 +1,6 @@
 This file is !potato.
 one more line!
 We're gonna celebrate!
-rehrthstjtyajy
-aethartenjyjateyjwymwrm
-eatnagnryhmrwnywrms
-adgnrgnhyabndtyrmnryh
-dgnnrydnsydnrmsuy
-tanyhnrymnsfuymufm
+{
+	rgetbetb
+}
